eeebea457d408927826943dcad9ad0c7aeff059f
Tirawat Simlemkim <tirawat.scu97@gmail.com>
2022-10-24 15:39:24 +0700

    first commit

---
+++ Master.py
@@ -0,0 +1,31 @@
+#import Pyqt5 modules
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+#import window.py class
+from window import Ui_MainWindow
+#import other modules
+import os
+import sys
+import subprocess
+import time
+import threading
+import shutil
+import re
+import datetime
+import csv
+
+class MyApp(QMainWindow):
+    def __init__(self, parent=None):
+        super(MyApp, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    myapp = MyApp()
+    myapp.show()
+    sys.exit(app.exec_())
