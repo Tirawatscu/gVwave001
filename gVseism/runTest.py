7c71f9cc86d92a4c450d705564bcdf85567acf51
tirawatscu <tirawat.scu97@gmail.com>
2022-11-29 21:03:28 +0700

    Runtest function

---
+++ gVseism/runTest.py
@@ -0,0 +1,57 @@
+import  RPi.GPIO as GPIO
+import pyadda
+from adc_consts import *
+import time
+import numpy as np
+import csv
+import sys
+
+# Raspberry pi pin numbering setup
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+PIN_DRDY = 11
+GPIO.setup(PIN_DRDY, GPIO.IN)
+
+# define gain, sampling rate, and scan mode
+gain = ADS1256_GAIN['1']
+samplingRate = ADS1256_DRATE['30000']
+scanMode = ADS1256_SMODE['DIFFERENTIAL']
+
+def interruptInterpreter(ch):
+	pyadda.collectData()
+	volts = pyadda.readAllChannelsVolts(adcChannels)
+	data.append([timeStamp, volts[0], volts[1], volts[2], volts[3]])
+
+# setup ads1256 chip
+pyadda.startADC(gain, samplingRate, scanMode)
+
+
+meanSampleRate = 0
+counter = 0
+adcChannels = 8 - 4 * scanMode
+#Create a numpy array to store the data 4 channels x 1000 samples
+data = []
+timeStamp = 0
+startTime = time.time()
+prevTime = time.time()
+GPIO.add_event_detect(PIN_DRDY, GPIO.FALLING)
+noSample = int(sys.argv[1])
+print(noSample)
+
+while True:
+	trig = time.time()
+	if trig - prevTime >= 1/256:
+		meanSampleRate += 1/(trig-prevTime)
+		timeStamp += trig - prevTime
+		prevTime = time.time()
+		interruptInterpreter(PIN_DRDY)
+		counter += 1
+		
+	if counter == noSample:
+		print(meanSampleRate/noSample)
+		break
+
+#Write data to a csv file
+with open('test_data.csv', 'w') as f:
+	writer = csv.writer(f)
+	writer.writerows(data)
