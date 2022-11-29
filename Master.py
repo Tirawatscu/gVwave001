631db26a18808e1a4d8c7c3ccd507ff995e93273
tirawatscu <tirawat.scu97@gmail.com>
2022-11-29 21:03:58 +0700

    Runtest function

--- Master.py
+++ Master.py
@@ -257,7 +257,7 @@ class MyApp(QMainWindow):
     #-------------------------------------------------#
 
     def runTest(self):
-        subprocess.check_call(['sudo python /pyadda/runTest.py', '2000'])
+        subprocess.check_call(['sudo python /gVseism/runTest.py', '2000'])
 
     #-------------------------------------------------#
 
