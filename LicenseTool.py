5f152293a9a2861447bc01e4dc1aa973231ea5f7
tirawatscu <tirawat.scu97@gmail.com>
2023-03-07 00:12:19 +0700

    Scaling Chart

--- LicenseTool.py
+++ LicenseTool.py
@@ -3,33 +3,26 @@ import subprocess
 import os
 
 def encode(serial, password):
-    # Encode the serial number and password as bytes
     serial_bytes = serial.encode()
     password_bytes = password.encode()
 
-    # XOR the serial number and password bytes together
     encoded_bytes = bytearray(len(serial_bytes))
     for i in range(len(serial_bytes)):
         encoded_bytes[i] = serial_bytes[i] ^ password_bytes[i % len(password_bytes)]
 
-    # Encode the resulting bytes as a base64 string
     encoded_str = base64.b64encode(encoded_bytes).decode()
 
     return encoded_str
 
 def decode(encoded, password):
-    # Decode the base64-encoded string as bytes
     encoded_bytes = base64.b64decode(encoded)
 
-    # Decode the password as bytes
     password_bytes = password.encode()
 
-    # XOR the encoded bytes and password bytes together
     serial_bytes = bytearray(len(encoded_bytes))
     for i in range(len(encoded_bytes)):
         serial_bytes[i] = encoded_bytes[i] ^ password_bytes[i % len(password_bytes)]
 
-    # Decode the resulting bytes as a string
     serial = serial_bytes.decode()
 
     return serial
@@ -37,14 +30,9 @@ def decode(encoded, password):
 
 
 def check_key(password):
-    # Check password length
-    
     if len(password) < 16:
         return False
-    
-    # Get the serial number of the Raspberry Pi
     serial = subprocess.run(["cat", "/sys/firmware/devicetree/base/serial-number"], stdout=subprocess.PIPE, universal_newlines=True).stdout.strip().split("\n")[-1].split(":")[-1].strip()
-    #print(f"Serial number: {serial}")
     # Decode the key and compare it to the serial number
     decoded = decode("eVd9SHlJdVooTDReLCIuC0k=", password)
     if decoded == serial:
